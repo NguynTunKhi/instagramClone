85a1d3cfe10159d5c8420fe7b1ffdb6df6d8b8dd
NguynTunKhi <khaint.bi11-116@st.usth.edu.vn>
2023-01-10 15:08:08 +0700

    Update ApiClient.java

--- AndroidStudioProjects/app/src/main/java/com/usth/instagramclone/Activities/ApiClient.java
+++ AndroidStudioProjects/app/src/main/java/com/usth/instagramclone/Activities/ApiClient.java
@@ -1,15 +1,10 @@
 package com.usth.instagramclone.Activities;
 
-import android.annotation.SuppressLint;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 
-import android.support.v7.app.AppCompatActivity;
+
 
 import com.usth.instagramclone.API.MyAPI;
 
